4cab5f04a62db8c2fb9e17726124db54a2295753
CesarConcep <cesarconcepcioncamilo@gmail.com>
2022-02-02 13:32:26 +0100

    add new functions to magic.js

--- js/magic.js
+++ js/magic.js
@@ -1,12 +1,23 @@
-function pressFunction(){
+function pressFunction() {
     console.log("sfdfj");
+    // DOM
+    var div = document.getElementById("div_magic");
+    div.classList.add("my_class");
+    console.log(div);
+    var div2 = document.getElementsByTagName("div");
+    console.log(div2);
+
+    var second_div = document.querySelector("#second_div");
+    console.log(second_div);
+
+    console.log($("#third_div"));
 }
 
-function mouseOverFunction(){
-    console.log("fsdfsdfsdfsdfsdf");
+function mouseOverFunction() {
+    console.log("adfsdfs");
 }
 
-// DOM
-var div = document.getElementsByClassName("div_magic");
-//div__.classList.add("my_class");
-console.log(div);
+function $(selector){
+    return document.querySelectorAll(selector);
+}
+
